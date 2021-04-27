e55ac368057ba40dc16b432f6b99880a1dd534c0
Ines <ines.-sosa@hotmail.com>
2021-04-27 20:33:37 -0300

    desafio 2 parte 1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# IntroPV-Desafio1
+# IntroPV-Desafio2
+
+Sobre el desafio anterior agregar los siguientes items:
+
+- Que el jugador pueda disparar proyectiles con direccion, velocidad y que el mismo desaparezca pasado un tiempo (DONE)
+- Que existan 'Torretas' que disparen a la posicion donde se encuentra el jugador (To Do)
--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -2,9 +2,29 @@ extends Sprite
 
 export (float) var speed : float
 
+onready var arm = $Arm
+
+func initialize(projectile_container):
+	arm.container = projectile_container
+
 func _process(delta):
+	
+	var leftLimit : int = 0
+	var rightLimit:int = 500
+	
 	var rightPressed : bool = Input.is_action_pressed("right")
 	var leftPressed  : bool = Input.is_action_pressed("left")
-	
 	var direction : int = int(rightPressed) - int(leftPressed)
-	position.x += direction * speed * delta
+	var newPosition : float =position.x +  (direction * speed * delta)
+	
+	var mouse_position:Vector2 = get_local_mouse_position()
+	arm.rotation = mouse_position.normalized().angle()
+	
+	if Input.is_action_just_pressed("fire"):
+		arm.fire()
+	
+	if newPosition < rightLimit || newPosition > leftLimit:
+		position.x = newPosition
+
+
+	
