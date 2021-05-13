ae37870e6389989c943fe5ea21a4adb31a839ae6
Ines <ines.-sosa@hotmail.com>
2021-05-13 19:32:32 -0300

    resubiendo...

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -1,10 +1,63 @@
-extends Sprite
+extends KinematicBody2D
 
-export (float) var speed : float
+onready var cannon = $Cannon
+onready var sprite = $Sprite
 
-func _process(delta):
-	var rightPressed : bool = Input.is_action_pressed("right")
-	var leftPressed  : bool = Input.is_action_pressed("left")
+export (float) var ACCELERATION:float = 20.0
+export (float) var H_SPEED_LIMIT:float = 600.0
+export (float) var FRICTION_WEIGHT:float = 0.1
+export (float) var JUMP_VELOCITY:float = 400
+
+var velocity:Vector2 = Vector2.ZERO
+var projectile_container
+
+
+func initialize(projectile_container):
+	self.projectile_container = projectile_container
+	cannon.projectile_container = projectile_container
+
+func _physics_process(delta):
 	
-	var direction : int = int(rightPressed) - int(leftPressed)
-	position.x += direction * speed * delta
+	# Cannon rotation
+	var mouse_position:Vector2 = get_global_mouse_position()
+	cannon.look_at(mouse_position)
+	
+	# Cannon fire
+	self._fire()
+	
+	self._flipPlayer()
+	
+	# Player movement
+	var h_movement_direction:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	
+	if h_movement_direction != 0:
+		velocity.x = clamp(velocity.x + (h_movement_direction * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
+	else:
+		velocity.x = lerp(velocity.x, 0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
+	
+	move_and_slide(Vector2.DOWN, Vector2(0,-1))
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y -= JUMP_VELOCITY
+	else:
+		velocity.y += JUMP_VELOCITY / 50
+		
+	move_and_slide(velocity)
+
+func notify_hit():
+	pass
+	
+func _fire():
+	if Input.is_action_just_pressed("fire_cannon"):
+		if projectile_container == null:
+			projectile_container = get_parent()
+			cannon.projectile_container = projectile_container
+		cannon.fire()
+		
+func _flipPlayer():
+	if Input.is_action_just_pressed("move_left"):
+		sprite.flip_h = true
+		cannon.flip_h = true
+	if Input.is_action_just_pressed("move_right"):
+		sprite.flip_h = false
+		cannon.flip_h = false
