afba4f2057b82bb880aa35c49be6ca356b95c0fa
Ines <ines.-sosa@hotmail.com>
2021-05-24 20:24:10 -0300

    Sonidos para disparos y jugador

---
+++ entities/player/Cannon.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+onready var cannon_tip = $CannonTip
+onready var cannonBlast = $CannonBlast
+
+export (PackedScene) var projectile_scene:PackedScene
+
+var projectile_container
+var proj_instance 
+
+func fire():
+	proj_instance = projectile_scene.instance()
+	proj_instance.initialize(projectile_container, cannon_tip.global_position, global_position.direction_to(cannon_tip.global_position))
+	cannonBlast.play()
+
+func add_collision_exception_to_projectile(object):
+	proj_instance.add_collision_exception_to_projectile(object)
---
+++ entities/player/PlayerOutput.gd
@@ -0,0 +1,8 @@
+extends AudioStreamPlayer
+
+#Audio files:
+onready var jump = load("res://Audio/Sfx/Jump.ogg") 
+onready var roll = load("res://Audio/Sfx/Roll.ogg") 
+onready var footsteps = [load("res://Audio/Sfx/Leaves1.ogg"), load("res://Audio/Sfx/Leaves2.ogg")] 
+onready var gameOver = load("res://Audio/Bgm/gameOver.ogg")
+
